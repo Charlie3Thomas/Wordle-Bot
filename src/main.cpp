1e149f3cb950169e056f5b1ad1b6ed168fb5b0b8
Charlie <charlie3thomas.work@gmail.com>
2022-04-18 21:53:32 +0100

    [WORDLE-BOT] Reading from file

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,108 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <array>
 #include <vector>
+#include <random>
 
+std::array<int, 5> compare(std::string _sol, std::string _sup);
+void eliminate_supports(std::string _guess, std::array<int, 5> _result);
 
 int main()
 {
+	std::ifstream ifs_w_sol("D:/Code/Wordle-Bot/resources/wordle_answers.txt");
+	std::ifstream ifs_w_sup("D:/Code/Wordle-Bot/resources/wordle_guesses.txt");
+	std::vector<std::string> wordle_solutions;
+	std::vector<std::string> wordle_supports;
 	std::string word;
-	std::vector<std::string> wordle_answers;
-	std::vector<std::string> wordle_guesses;
-	std::ifstream ifs_w_ans("D:/Code/Wordle-Bot/resources/wordle_answers.txt");
-	std::ifstream ifs_w_gss("D:/Code/Wordle-Bot/resources/wordle_guesses.txt");
 
-	if (ifs_w_ans.is_open() == false)
+	if (ifs_w_sol.is_open() == false)
 	{
-		std::cout << "Wordle answers file not found!" << std::endl;
-	}
-	else
-	{
-		std::cout << "Wordle answers file found!" << std::endl;
+		std::cout 
+			<< "Wordle answers file not found!" 
+			<< std::endl;
 	}
 
-	if (ifs_w_gss.is_open() == false)
+	if (ifs_w_sup.is_open() == false)
 	{
-		std::cout << "Wordle guesses file not found!" << std::endl;
+		std::cout 
+			<< "Wordle guesses file not found!" 
+			<< std::endl;
 	}
-	else
-	{
-		std::cout << "Wordle guesses file found!" << std::endl;
-	}
-
 	
-	while (ifs_w_ans >> word)
+	while (ifs_w_sol >> word)
 	{
-		wordle_answers.push_back(word);
+		wordle_solutions.push_back(word);
 	}
 
-	while (ifs_w_gss >> word)
+	while (ifs_w_sup >> word)
 	{
-		wordle_guesses.push_back(word);
+		wordle_supports.push_back(word);
 	}
 
-	std::cout << "PRINTING ANSWERS" << std::endl;
-	for (std::string answer : wordle_answers)
-	{		
-		std::cout << answer << std::endl;
+	std::vector<int> wdl_sup(wordle_supports.size());
+
+
+	/*
+		Select a random solution and starting support.
+	*/
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> distr_sol(0, wordle_solutions.size());
+	std::uniform_int_distribution<> distr_sup(0, wordle_supports.size());
+	std::string answer = wordle_solutions[distr_sol(gen)];
+	std::string guess = wordle_supports[distr_sup(gen)];
+
+	std::cout 
+		<< "Solution is: "
+		<< answer 
+		<< std::endl;
+
+	std::cout 
+		<< "Support is: "
+		<< guess 
+		<< std::endl;
+	
+	std::array<int, 5> comparison_result = compare(answer, guess);
+
+	for (int i = 0; i < comparison_result.size(); i++)
+	{
+		std::cout
+			<< comparison_result[i]
+			<< " ";
 	}
 
-	std::cout << "PRINTING GUESSES" << std::endl;
-	for (std::string guess : wordle_guesses)
-	{		
-		std::cout << guess << std::endl;
-	}
+	std::cout << std::endl;
 
 	return 0;
+}
+
+std::array<int, 5> compare(std::string _sol, std::string _sup)
+{
+	return std::array<int, 5> {0, 0, 0, 0, 0};
+}
+
+void eliminate_supports(std::string _guess, std::array<int, 5> _result)
+{
+	/*
+		Example result 01002:
+			First letter is not used.
+			Second letter is used, but in another position.
+			Third letter is not used.
+			Fourth letter is not used.
+			Fifth letter is used in this position.
+
+		If there are any "2" results in _result, eliminate all supports that do not
+		have this letter in this position.
+
+		Take in string
+		Take in _result array
+		if any position in _result contains 2, remove all words that do not have letters in the same places
+		if any position in _result contains 1, remove all words that do not contain those letters
+		if any position in _result contains 0, remove all words that contain those letters
+
+		For string.size -> loop through each char
+
+	
+	*/
 }
